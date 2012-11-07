2d41753abd5f0fd94428b3f558a362a6db6b8047
Michael Yeaple <mdy7643@g.rit.edu>
2012-11-07 16:28:52 -0500

    Added comments and fixed skeletons for the strategies.

--- RefactoringProject/src/FinalFrameStrategy.java
+++ RefactoringProject/src/FinalFrameStrategy.java
@@ -1,28 +1,33 @@
 
 /**
+ *  NormalFrameStrategy.java
  * 
+ * This class is meant to handle PinsetterEvents for Lane.
+ * This strategy should be used for frames 1-9.
+ * 
+ * @author Michael Yeaple
  */
 
-/**
- * @author mhickson
- *
- */
 public class FinalFrameStrategy implements LaneStrategy {
 
 	/**
-	 * 
+	 * Default constructor for the FinalFrameStrategy.
+	 * Should be empty.
 	 */
 	public FinalFrameStrategy() {
-		// TODO Auto-generated constructor stub
+		
 	}
 
-	/* (non-Javadoc)
-	 * @see LaneStrategy.LaneStrategy#execute()
+	/**
+	 * Method for handling PinsetterEvents sent from Lane.
+	 * Specifically, this will mark the score and set whether
+	 * the bowler can throw again or not.
+	 * 
+	 * @param pse - The PinsetterEvent to be handled.
+	 * @param curLane - The Lane that the PinsetterEvent comes from.
 	 */
-	@Override
-	public void execute() {
-		// TODO Auto-generated method stub
-
+	public void handlePinsetterEvent(PinsetterEvent pse, Lane curLane) {
+		
 	}
 
 }
--- RefactoringProject/src/LaneStrategy.java
+++ RefactoringProject/src/LaneStrategy.java
@@ -1,5 +1,21 @@
-
+/**
+ * LaneStrategy.java
+ * 
+ * Defines the interface that each of the concrete lane strategies must adhere to.
+ * The main purpose for this class is to define an interface to handle PinsetterEvents for Lane.
+ * 
+ * @author - Michael Yeaple
+ */
 
 public interface LaneStrategy {
-	public void execute();
+	
+	/**
+	 * Method for handling PinsetterEvents sent from Lane.
+	 * Specifically, this will mark the score and set whether
+	 * the bowler can throw again or not.
+	 * 
+	 * @param pse - The PinsetterEvent to be handled.
+	 * @param curLane - The Lane that the PinsetterEvent comes from.
+	 */
+	public void handlePinsetterEvent(PinsetterEvent pse, Lane curLane);
 }
